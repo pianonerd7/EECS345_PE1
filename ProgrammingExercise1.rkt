690d298a51aad9fd1deeec5c85ac1d16750694ac
pianonerd7 <jxh604@case.edu>
2016-01-22 14:58:34 -0500

    2nd question done

--- ProgrammingExercise1.rkt
+++ ProgrammingExercise1.rkt
@@ -2,10 +2,18 @@
 ;EECS 345 Programming Exercise 1
 
 ;inorder
-(define inorder
+(define inorder?
   (lambda (l)
     (cond
       ((null? l)#t)
       ((null?(cdr l))#t)
       ((>(car l)(car(cdr l)))#f)
       (else (inorder (cdr l))))))
+
+;dotproduct
+(define dotproduct
+  (lambda (l1 l2)
+    (cond
+      ((null? l1) 0)
+      ((null? l2) 0)
+      (else(+(*(car l1)(car l2))(dotproduct(cdr l1)(cdr l2)))))))
