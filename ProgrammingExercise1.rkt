a8b801e5df5779e203457a1e7e80bc75bf1f3ba2
pianonerd7 <jxh604@case.edu>
2016-01-22 22:00:06 -0500

    skipped 3. did 4

--- ProgrammingExercise1.rkt
+++ ProgrammingExercise1.rkt
@@ -17,3 +17,17 @@
       ((null? l1) 0)
       ((null? l2) 0)
       (else(+(*(car l1)(car l2))(dotproduct(cdr l1)(cdr l2)))))))
+
+;squareroot
+;(define squareroot
+;  (lambda (value iter)
+;    (cond
+
+;removesubsequence
+(define removesubsequence
+  (lambda (l1 l2)
+    (cond
+      ((null? l2)'())
+      ((null? l1)l2)
+      ((eq? (car l1)(car l2))(removesubsequence(cdr l1)(cdr l2)))
+      (else (cons (car l1)(removesubsequence l1 (cdr l2)))))))
